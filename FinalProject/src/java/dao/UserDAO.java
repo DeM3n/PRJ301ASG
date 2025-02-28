fb73fa6b7f2c1f563faae3364cf97f6dccd6a045
Cuong Lam-Quoc <cuonglam.fpt@gmail.com>
2025-02-28 10:26:07 +0700

    dao

---
+++ FinalProject/src/java/dao/UserDAO.java
@@ -0,0 +1,41 @@
+package dao;
+
+import java.util.List;
+
+/**
+ *
+ * @author no-solace
+ */
+public class UserDAO implements IDAO<Object, String> {
+
+    @Override
+    public boolean create(Object entity) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Object> readAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object readById(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean update(Object entity) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean delete(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Object> search(String searchTerm) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
--- FinalProject/src/java/utils/DBUtils.java
+++ FinalProject/src/java/utils/DBUtils.java
@@ -1,8 +1,11 @@
 /*
     
  */
-
 package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  *
@@ -10,4 +13,15 @@ package utils;
  */
 public class DBUtils {
 
+    private static final String DB_Name = "sisyphus";
+    private static final String DB_Username = "sa";
+    private static final String DB_Password = "12345";
+
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+        Connection con = null;
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        String url = "jdbc:sqlserver://localhost:1433;databaseName=" + DB_Name;
+        con = DriverManager.getConnection(url, DB_Username, DB_Password);
+        return con;
+    }
 }
